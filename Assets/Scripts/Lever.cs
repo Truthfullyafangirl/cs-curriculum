1d98e0b7a0cca2b721a21b16705e548f0eadd2d9
Truthfullyafangirl <144268552+Truthfullyafangirl@users.noreply.github.com>
2024-02-05 10:56:45 -0800

    g

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,25 @@ public class Lever : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         
     }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Lever"))
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Open();
+            }
+        }
+    }
+
+    void Open()
+    {
+        GameObject myObject = GameObject.Find("Pixel door");
+        myObject.SetActive(false);
+    }
+    
 }
